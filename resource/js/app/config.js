906bc1d5660b82a0f13725a86941297608d53d2e
javaguo <2904658645@qq.com>
2019-03-30 17:43:04 +0800

    配置文件。

---
+++ resource/js/app/config.js
@@ -0,0 +1,14 @@
+const SERVER_URL = 'http://39.104.162.221:8102/per/';
+// const SERVER_URL = 'http://localhost:8101/compre/';
+const TEST_LOGIN_NAME = 'zjg';
+const TEST_LOGIN_PWD = 'c696360395cb2338d78f9682de683f7e';
+
+const PAGE_SIZE = 10;
+
+
+export default {
+	SERVER_URL,
+	TEST_LOGIN_NAME,
+	TEST_LOGIN_PWD,
+	PAGE_SIZE
+}
